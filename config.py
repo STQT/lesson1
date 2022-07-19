021fb7cd6f9982eb90a1db1181def4a2b01c4fc3
Kairat Sultan <garri.sultonov@mail.ru>
2022-07-19 20:41:37 +0500

    setting webhook

--- config.py
+++ config.py
@@ -4,5 +4,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DEBUG = os.getenv('DEBUG')
+
 TOKEN = os.getenv('TELEGRAM_TOKEN')
 OWM_TOKEN = os.getenv('OWM_TOKEN')
+
+# webhook settings
+WEBHOOK_HOST = os.getenv('HEROKU_APP_URL')
+WEBHOOK_PATH = '/'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'  # or ip
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
+async def on_startup(dp):
+    await dp.bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+    print("Bot started")
+
+
+async def on_shutdown(dp):
+    print("Bot stopped")
+    await dp.bot.delete_webhook()
+
+    # Close DB connection (if used)
+    await dp.storage.close()
+    await dp.storage.wait_closed()
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from pprint import pprint
 from utils import getter, get_nationalize  # 3
-
-from config import TOKEN, OWM_TOKEN
+from aiogram.utils.executor import start_webhook
+from config import *
 from aiogram import Bot, Dispatcher, executor, types
-
 
 # Initialize bot and dispatcher
 bot = Bot(token=TOKEN)
@@ -21,4 +20,17 @@ async def some_handler(message: types.Message):
     name = await message.answer(get_nationalize(message.text))
     print(name)
 
-executor.start_polling(dp, skip_updates=True)
+
+if __name__ == '__main__':
+    if DEBUG:
+        executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
+    else:
+        start_webhook(
+            dispatcher=dp,
+            webhook_path=WEBHOOK_PATH,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            skip_updates=True,
+            host=WEBAPP_HOST,
+            port=WEBAPP_PORT,
+        )
